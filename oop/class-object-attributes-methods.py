5196ad55aad1ee36970a87f16948057aba7a5176
Harshavardhan I <harshavardhan_i@dell.com>
2021-07-05 14:29:07 +0530

    control flow, methods, funtions and oop done

---
+++ oop/class-object-attributes-methods.py
@@ -0,0 +1,12 @@
+	# • Class object attributes are same for any instance of the class
+	# • 
+	# • self keyword is a reference to a particular instance of a class
+	# • method is a func inside the class and works with the object in some way
+	# • self keyword connects the method to the object
+	# • 
+	# • attributes are variables within the class object
+	# • attributes don’t need to be executed so they are called without parenthesis
+	# • 
+	# • Class object variable can be accessed via class name instead of self
+	
+	
